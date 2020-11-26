170fd58954d5f1e4a7a653ae30207af01d73d365
manuelkas <manuelkas@riseup.net>
2020-11-26 09:15:31 -0500

    Exercise 3.13

---
+++ src/Ch03/ex03_13/Employee.java
@@ -0,0 +1,56 @@
+// 3.13 (Employee Class) Create a class called Employee that includes three instance variables
+// —a first name (type String), a last name (type String) and a monthly salary (double).
+// Provide a constructor that initializes the three instance variables.
+// Provide a set and a get method for each instance variable.
+// If the monthly salary is not positive, do not set its value.
+// Write a test app named EmployeeTest that demonstrates class Employee’s capabilities.
+// Create two Employee objects and display each object’s yearly salary.
+// Then give each Employee a 10% raise and display each Employee’s yearly salary again.
+
+package Ch03.ex03_13;
+
+public class Employee {
+   private String firstName;  // instance variable
+   private String lastName;  // instance variable
+   private double monthlySalary;  // instance variable
+
+   // Employee constructor that receives three parameters
+   public Employee(String firstName, String lastName, double monthlySalary) {
+      this.firstName = firstName; // asign firstName to instance variable firstName
+      this.lastName = lastName; // asign lastName to instance variable lastName
+
+      // validate that the monthlySalary is greater than 0.0; if it's not,
+      // instance variable monthly don't set its value.
+      if (monthlySalary > 0.0){
+         this.monthlySalary = monthlySalary;
+      }
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public double getMonthlySalary() {
+      return monthlySalary;
+   }
+
+   public void setMonthlySalary(double monthlySalary) {
+      // validate that the monthlySalary is greater than 0.0; if it's not,
+      // instance variable monthly don't set its value.
+      if (monthlySalary > 0.0){
+         this.monthlySalary = monthlySalary;
+      }
+   }
+}
---
+++ src/Ch03/ex03_13/EmployeeTest.java
@@ -0,0 +1,28 @@
+// Write a test app named EmployeeTest that demonstrates class Employee’s capabilities.
+// Create two Employee objects and display each object’s yearly salary.
+// Then give each Employee a 10% raise and display each Employee’s yearly salary again.
+package Ch03.ex03_13;
+
+public class EmployeeTest {
+   public static void main(String[] args) {
+      Employee employee1 = new Employee("Manuel", "Castro", 1000);
+      Employee employee2 = new Employee("Yira", "Pérez", 4980);
+
+      // display yearly salary of each object
+      System.out.printf("%s %s's yearly salary is: $ %.2f%n",
+         employee1.getFirstName(), employee1.getLastName(), employee1.getMonthlySalary() * 12);
+      System.out.printf("%s %s's yearly salary is: $ %.2f%n%n",
+         employee2.getFirstName(), employee2.getLastName(), employee2.getMonthlySalary() * 12);
+
+      // raised monthly salary by 10%
+      employee1.setMonthlySalary(employee1.getMonthlySalary() * 1.10);
+      employee2.setMonthlySalary(employee2.getMonthlySalary() * 1.10);
+
+      // display new yearly salaries
+      System.out.println("Salaries have been raised 10%.");
+      System.out.printf("%s %s's yearly salary is: $ %.2f%n",
+         employee1.getFirstName(), employee1.getLastName(), employee1.getMonthlySalary() * 12);
+      System.out.printf("%s %s's yearly salary is: $ %.2f%n",
+         employee2.getFirstName(), employee2.getLastName(), employee2.getMonthlySalary() * 12);
+   }
+}
