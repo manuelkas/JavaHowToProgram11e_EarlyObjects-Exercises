6949e3d119da31bd033db106c72ca14328e7b299
manuelkas <manuelkas@riseup.net>
2020-11-27 11:32:48 -0500

    Exercise 3.16

--- src/Ch03/ex03_16/TargetHeartRateCalculator.java
+++ src/Ch03/ex03_16/TargetHeartRateCalculator.java
@@ -1,3 +1,16 @@
+// exercise 3.16: TargetHeartRateCalculator
+// Create a class called HeartRates. The class attributes should include
+// the person’s first name, last name and date of birth
+// (consisting of separate attributes for the month, day and year of birth).
+// Your class should have a constructor that receives this data as parameters.
+// For each attribute provide set and get methods.
+// The class also should include a method that calculates and returns the person’s age (in years),
+// a method that calculates and returns the person’s maximum heart rate and a method that calculates
+// and returns the person’s target heart rate.
+// Write a Java app that prompts for the person’s information, instantiates an object
+// of class HeartRates and prints the information from that object—including the person’s
+// first name, last name and date of birth—then calculates and prints the person’s age in (years),
+// maximum heart rate and target-heart-rate range.
 package Ch03.ex03_16;
 
 import java.util.Scanner;
