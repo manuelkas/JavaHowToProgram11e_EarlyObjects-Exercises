4465035db174a4b6f0dc3605fe5d83fb8696b0c3
manuelkas <manuelkas@riseup.net>
2020-12-06 12:55:51 -0500

    Updated

--- src/Ch04/ex04_34/MultiplesOf2WithInfiniteLoop.java
+++ src/Ch04/ex04_34/MultiplesOf2WithInfiniteLoop.java
@@ -2,7 +2,7 @@
 // (Multiples of 2 with an Infinite Loop) Write an application that keeps displaying
 // in the command window the multiples of the integer 2—namely, 2, 4, 8, 16, 32, 64, and so on.
 // Your loop should not terminate (i.e., it should create an infinite loop).
-// What happens when you run this program?
+//  What happens when you run this program?
 
 package Ch04.ex04_34;
 
