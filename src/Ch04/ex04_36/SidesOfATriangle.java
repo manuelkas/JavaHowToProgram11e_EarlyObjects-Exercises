bf914c20a6230b4f5c7dde7b59a58127c56b12e2
manuel <manuelkas@riseup.net>
2020-12-06 14:56:38 -0500

    Updating

---
+++ src/Ch04/ex04_36/SidesOfATriangle.java
@@ -0,0 +1,36 @@
+// Exercise 4.36: SidesOfATriangle
+// Write an application that reads three nonzero values entered by the user
+// and determines and prints whether they could represent the sides of a triangle.
+
+package Ch04.ex04_36;
+
+import java.util.Scanner;
+
+public class SidesOfATriangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first positive number: ");
+        double a = input.nextDouble();
+        System.out.print("Enter second positive number: ");
+        double b = input.nextDouble();
+        System.out.print("Enter third positive number: ");
+        double c = input.nextDouble();
+
+        if ((a + b) > c) {
+            double difference = (a - b);
+            if ((a - b) < 0) {
+                difference = b - a;
+            }
+            if (difference < c){
+                System.out.println("These numbers could represent the sides of a triangle.");
+            }
+            else {
+                System.out.println("These numbers could not represent the sides of a triangle.");
+            }
+        }
+        else {
+            System.out.println("These numbers could not represent the sides of a triangle.");
+        }
+    }
+}
